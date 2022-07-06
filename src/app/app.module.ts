6653a72f8bed43bce027bbac0f5cdc20627f5614
Jasmine Garcia <garciajasminejoy@gmail.com>
2022-07-06 18:19:20 +0800

    add contact component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookReviewsComponent } from './components/book-reviews/book-reviews.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
 import { MainComponent } from './components/main/main.component';
 import { MovieReviewsComponent } from './components/movie-reviews/movie-reviews.component';
-import { BookReviewsComponent } from './components/book-reviews/book-reviews.component';
 import { TopStoriesComponent } from './components/top-stories/top-stories.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
@@ -17,12 +20,16 @@ import { HomeComponent } from './components/home/home.component';
     MovieReviewsComponent,
     BookReviewsComponent,
     TopStoriesComponent,
-    HomeComponent
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]

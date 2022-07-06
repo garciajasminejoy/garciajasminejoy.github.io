6f2875917dc2461d3344a102992f1fc7e3341d79
Jasmine Garcia <garciajasminejoy@gmail.com>
2022-07-06 18:18:14 +0800

    contact component

---
+++ src/app/components/contact/contact.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'nyx-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent implements OnInit {
+  contactForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.contactForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      title: ['', Validators.required],
+      message: ['', Validators.required],
+    });
+  }
+
+  submitContactForm(): void {
+    if (!this.contactForm.valid) {
+      console.error('Invalid form.');
+    }
+  }
+
+  get firstName(): AbstractControl {
+    return this.contactForm.controls['firstName'];
+  }
+
+  get lastName(): AbstractControl {
+    return this.contactForm.controls['lastName'];
+  }
+
+  get email(): AbstractControl {
+    return this.contactForm.controls['email'];
+  }
+
+  get title(): AbstractControl {
+    return this.contactForm.controls['title'];
+  }
+
+  get message(): AbstractControl {
+    return this.contactForm.controls['message'];
+  }
+
+}
